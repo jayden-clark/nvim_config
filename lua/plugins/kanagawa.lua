f0bd2fbdea433d7a34813d74446e3d1c8b2b38b7
jantarktic <jaydenclark@outlook.com.au>
2024-02-29 23:00:12 +1100

    removed line number bg

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,11 +1,10 @@
 return {
-        "rebelot/kanagawa.nvim",
-        lazy = false,
-        priority = 1000,
-        name = "kanagawa",
-        config = function()
-            vim.cmd.colorscheme "kanagawa"
-        end
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	name = "kanagawa",
+	config = function()
+		vim.cmd.colorscheme("kanagawa")
+    vim.api.nvim_set_hl(0, 'LineNr', {bg = 'None'})
+	end,
 }
-
-
